1f2ed3033f1a6e3ce781f88f6a0b44fdeaccb803
Beisenbek Baisakov <beisenbek@gmail.com>
2016-01-23 12:58:25 +0600

    fix

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace Example1
 {
+
+    class Point
+    {
+        public int x;
+        public int y;
+        public Point()
+        {
+            x = 10;
+            y = 10;
+        }
+        public Point(int x)
+        {
+            this.x = x;
+            y = 10;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Point p = new Point(20);
+
+            Console.WriteLine(p.x + " " + p.y);
         }
     }
 }
